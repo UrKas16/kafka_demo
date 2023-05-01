70f20fa832e6cffa1d29827e8d1851b2e6612db7
UrKas16 <kasyurvik568@gmail.com>
2023-05-01 18:47:55 +0300

    first commit

---
+++ src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+@EnableKafka
+public class KafkaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoApplication.class, args);
+    }
+
+}
